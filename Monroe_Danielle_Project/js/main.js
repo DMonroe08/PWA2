11386c3d8a4a794330a1eee2fa5775d425d56c13
dmonroe08 <dmonroe08@fullsail.edu>
2014-09-12 23:04:18 -0400

    Week 2 Project

--- Monroe_Danielle_Project/js/main.js
+++ Monroe_Danielle_Project/js/main.js
@@ -55,5 +55,60 @@
 })(jQuery); // end private scope
 
 
+/* jQuery for tabs */
+
+$('#tabs_info p').hide().ep(0);
+$('#tabs_info p:not(first)').hide();
+$('#tabs_nav li').click(function(e){
+    e.preventDefault();
+    $('#tabs p').hide();
+    $('#tabs_nav .current').removeClass("current");
+    $(this).addClass('current');
+    var clicked = $(this).find('a:first').attr('href');
+    $('#tabes ' + clicked).fadeIn('medium');
+
+})..eq(0).addClass('current');
 
 
+/* jQuery for Projects form */
+
+$('.modal').on('click', function(event){
+    event.preventDefault();
+    $('#overlay')
+        .fadeIn(485)
+        .find('#projectForm')
+        .fadeIn();
+
+});
+
+
+/* jQuery for add and status buttons */
+
+$('.status').hover(function(){
+    $(this).fadeTo(300, 3);
+});
+
+
+$('.add').hover(function(){
+    $(this).fadeOut(300);
+    $(this).fadeIn(300);
+});
+
+
+
+/* jQuery for Tool Tips */
+
+$('.mastTooltip').hover(function(){
+    var title = $(this).attr('title');
+    $(this).data('tipText',title).removeAttr('title');
+    $('<p class="toolTip"></p>).text(title).appendTo('body').fadeIn('slow);
+}, function(){
+    $(this).attr('title', $(this).data('tipText'));
+    $('.tooltip').remove();
+}).mousemove(function(e){
+        var mousex = e.pageX + 15;
+        var mousey = e.pageY + 25;
+        $('.tooldtip')
+            .css({top: mousey, left: mousex})
+
+});

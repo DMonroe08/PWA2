e5a9e6df835be7edee030963da90df2ae9bf6b64
dmonroe08 <dmonroe08@fullsail.edu>
2014-09-26 13:09:11 -0400

    projects push

--- wk4/js/main.js
+++ wk4/js/main.js
@@ -241,62 +241,62 @@
 
     });
 
-var projects = function(){
-
-$.ajax({
-url: 'xhr/get_projects.php',
-type: 'get',
-dataType: 'json',
-success: function(response){
-if(response.error){
-console.log(response.error);
-}else{
-
-for(var i=0, j=response.projects.length; i < j; i++){
-var result = response.projects[i];
-
-$(".projects").append(
-//'<div style="border:1px solid black">' +
-'<div id="sortable" class="ui-state-default">' +
-" <input class='projectid' type='hidden' value='" + result.id + "'>" +
-" Project Name: " + result.projectName + "<br>" +
-" Project Due Date: " + result.dueDate + "<br>" +
-" Project Description: " + result.projectDescription + "<br>" +
-" Project Status: " + result.status + "<br>"
-+ '<button class="deletebtn">Delete</button>'
-+ '<button class="editButton">Edit</button>'
-+ '</div> <br>'
-);
-};
-$('.deletebtn').on('click', function(e){
-var pid = $(this).parent().find(".projectid").val();
-console.log('test delete');
-$.ajax({
-url: 'xhr/delete_project.php',
-data: {
-projectID: pid
-},
-type: 'POST',
-dataType: 'json',
-success: function(response){
-console.log('Testing for success');
-
-if(response.error) {
-alert(response.error);
-} else {
-//console.log(result.id);
-window.location.assign("projects.html");
-};
-}
-});
-}); // End Delete
-
-
-}
-}
-})
-}
-projects();
+//var projects = function(){
+//
+//$.ajax({
+//url: 'xhr/get_projects.php',
+//type: 'get',
+//dataType: 'json',
+//success: function(response){
+//if(response.error){
+//console.log(response.error);
+//}else{
+//
+//for(var i=0, j=response.projects.length; i < j; i++){
+//var result = response.projects[i];
+//
+//$(".projects").append(
+////'<div style="border:1px solid black">' +
+//'<div id="sortable" class="ui-state-default">' +
+//" <input class='projectid' type='hidden' value='" + result.id + "'>" +
+//" Project Name: " + result.projectName + "<br>" +
+//" Project Due Date: " + result.dueDate + "<br>" +
+//" Project Description: " + result.projectDescription + "<br>" +
+//" Project Status: " + result.status + "<br>"
+//+ '<button class="deletebtn">Delete</button>'
+//+ '<button class="editButton">Edit</button>'
+//+ '</div> <br>'
+//);
+//};
+//$('.deletebtn').on('click', function(e){
+//var pid = $(this).parent().find(".projectid").val();
+//console.log('test delete');
+//$.ajax({
+//url: 'xhr/delete_project.php',
+//data: {
+//projectID: pid
+//},
+//type: 'POST',
+//dataType: 'json',
+//success: function(response){
+//console.log('Testing for success');
+//
+//if(response.error) {
+//alert(response.error);
+//} else {
+////console.log(result.id);
+//window.location.assign("projects.html");
+//};
+//}
+//});
+//}); // End Delete
+//
+//
+//}
+//}
+//})
+//}
+//projects();
 
     /* Projects Display */
   //  var projects = function(){
@@ -353,6 +353,19 @@ projects();
 //		});
 //	}
 //projects();
+
+$(".projects").append(
+
+'<div id="sortable" class="ui-state-default"> '+ //removed the closed div tag
+"<input class='projectID' type='hidden' value='" + result.id +"'>"+
+"Project Name: " + result.projectName + "<br>" +
+"Project Description: " + result.projectDescription + "<br>" +
+"Project Status: " +result.status + "<br>"
++'<button class="deleteButton">Delete</button>'
++'<button class="editButton">Edit</button>' //Added a t to both button tags
++'</div> <br>'
+);
+
 
                 ///* Code for Datepicker */
 //                
